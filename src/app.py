e498c2ff0cf07b0043adf4e78efb3ff0c19455b1
Lingjian Wang <superkerokero@outlook.com>
2018-06-26 23:45:56 +0900

    Added update functionality to the app.

--- src/app.py
+++ src/app.py
@@ -1,35 +1,78 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 import pandas as pd
-from cryptoAPI import load_history, sort_by_mode, make_df
+from coinmarketcap import Market
+from cryptocmd import CmcScraper
+import json
+from cryptoAPI import load_history, sort_by_mode, make_df, get_id_table
 
 
 def toCSV(event):
-    fname = "data/"+fn.get()+".csv"
+    fname = fn.get()+".csv"
     try:
-        hist, s2i, i2s = load_history("data/cmc_history.json")
+        hist, s2i, i2s = load_history("cmc_history.json")
         df = make_df(sort_by_mode(hist, st.get(), et.get(), opt.get()))
         df.to_csv(fname)
         messagebox.showinfo('info', "成功输出文件: {0}".format(fname))
     except ValueError:
-        messagebox.showinfo('info', "数值错误， 请检查输入区间是否合法")
+        messagebox.showinfo('info', "数值错误，请检查输入区间是否合法")
     except:
-        messagebox.showinfo('info', "未知错误")
+        messagebox.showinfo('info', "系统错误，如果第一次打开本app，请先点击右下角“更新数据”按钮进行数据更新")
 
 
 def toExcel(event):
-    fname = "data/"+fn.get()+".xlsx"
+    fname = fn.get()+".xlsx"
     try:
-        hist, s2i, i2s = load_history("data/cmc_history.json")
+        hist, s2i, i2s = load_history("cmc_history.json")
         df = make_df(sort_by_mode(hist, st.get(), et.get(), opt.get()))
         writer = pd.ExcelWriter(fname)
         df.to_excel(writer, 'Sheet1')
         writer.save()
         messagebox.showinfo('info', "成功输出文件: {0}".format(fname))
     except ValueError:
-        messagebox.showinfo('info', "数值错误， 请检查输入区间是否合法")
+        messagebox.showinfo('info', "数值错误，请检查输入区间是否合法")
     except:
-        messagebox.showinfo('info', "未知错误")
+        messagebox.showinfo('info', "系统错误，如果第一次打开本app，请先点击右下角“更新数据”按钮进行数据更新")
+
+
+def update(event):
+    messagebox.showinfo('info',
+                        """更新历史数据耗时较久，视网络环境需时20到40分钟，
+                        输出文件名下方的进度条显示当前的更新进度，
+                        背后的命令行窗口会输出目前处理中的加密货币ID和符号，
+                        coinmarkectcap目前共有1500多种加密货币""")
+    cmc = Market()
+    listing = [item['symbol'] for item in cmc.listings()['data']]
+    hist = dict()
+    step = 100 / len(listing)
+    for i, x in enumerate(listing):
+        scr = CmcScraper(x)
+        try:
+            header, data = scr.get_data()
+            ti = list()
+            pr = list()
+            for j, xx in enumerate(data):
+                if xx[4]:
+                    pr.append(xx[4])
+                    ti.append(xx[0])
+            hist[x] = dict(
+                t=ti,
+                p=pr
+            )
+        except Exception:
+            print("Invalid coin ssymbol: {0}".format(x))
+        print(i, x)
+        progress.step(step)
+        progress.update()
+    sym2id, id2sym = get_id_table()
+    outdata = dict(
+        history=hist,
+        s2i=sym2id,
+        i2s=id2sym
+    )
+    with open("cmc_history.json", "w") as f:
+        json.dump(outdata, f)
+    messagebox.showinfo('info', "成功更新历史数据")
 
 
 root = tk.Tk()
@@ -58,18 +101,25 @@ tk.Radiobutton(root,
 tk.Radiobutton(root,
                text="现价相对于指定期间低点涨幅", variable=opt, value=3).pack(anchor=tk.W)
 
-tk.Label(root, text="输出文件名(data文件夹内)", foreground='#000000',
+tk.Label(root, text="输出文件名(app根目录下)", foreground='#000000',
          background='#aaaaff').pack()
 fn = tk.StringVar()
 tk.Entry(root, width=50, textvariable=fn).pack()
 fn.set("output")
 
-btn1 = tk.Button(root, text="输出csv文件")
-btn1.bind("<Button-1>", toCSV)
-btn1.pack()
+progress = ttk.Progressbar(root, orient=tk.HORIZONTAL, length=200)
+progress.pack(fill=tk.X, padx=20)
 
-btn2 = tk.Button(root, text="输出excel文件")
+btn1 = tk.Button(root, text="输出csv")
+btn1.bind("<Button-1>", toCSV)
+btn1.pack(side=tk.LEFT)
+
+btn2 = tk.Button(root, text="输出excel")
 btn2.bind("<Button-1>", toExcel)
-btn2.pack()
+btn2.pack(side=tk.LEFT)
+
+btn3 = tk.Button(root, text="更新数据")
+btn3.bind("<Button-1>", update)
+btn3.pack(side=tk.RIGHT)
 
 root.mainloop()
